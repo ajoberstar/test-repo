da3e05117108473d0148d969e0f6a8e4cfd5486c
Andrew Oberstar <ajoberstar@gmail.com>
2014-04-12 21:08:20 -0500

    Adding change to push3.

--- src/main/java/com/ajoberstar/test/MyClass.java
+++ src/main/java/com/ajoberstar/test/MyClass.java
@@ -4,6 +4,7 @@ package org.ajoberstar.test;
  * Some docs.
  * Adding something to push.
  * Adding something to push.
+ * Adding something to push.
  */
 public class MyClass {
 	public static void main(String[] args) {
