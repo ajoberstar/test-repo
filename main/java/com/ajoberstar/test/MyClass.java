7e918a3d0f62686ddd8f11bc59cb78a22d5681ca
Andrew Oberstar <ajoberstar@gmail.com>
2015-05-30 15:47:48 -0500

    Publish of Github pages from Gradle.

---
+++ main/java/com/ajoberstar/test/MyClass.java
@@ -0,0 +1,25 @@
+/*
+ * Testing license.
+ */
+package org.ajoberstar.test;
+
+/**
+ * Some docs.
+ * Adding something to push.
+ * Adding something to push.
+ * Adding something to push.
+ * Adding something to push.
+ * Adding something to push.
+ * Adding something to push.
+ * Adding something to push.
+ * Adding something to push.
+ * Adding something to push.
+ * Adding something to push.
+ * Adding something to push.
+ * @since 0.3.0
+ */
+public class MyClass {
+	public static void main(String[] args) {
+		System.out.println("Executing...");
+	}
+}
