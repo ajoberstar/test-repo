a1c06937d2b7f36e9ab1c3da520b5d52532250d9
Andrew Oberstar <ajoberstar@gmail.com>
2016-12-17 12:27:13 -0600

    semicolons. duh

--- src/test/java/com/ajoberstar/MyTest.java
+++ src/test/java/com/ajoberstar/MyTest.java
@@ -5,11 +5,11 @@ import org.junit.Test;
 public class MyTest {
   @Test
   public void testSomething() {
-    assert 1 + 2 == 3
+    assert 1 + 2 == 3;
   }
 
   @Test
   public void testSomethingThatFails() {
-    assert 1 + 2 == 2
+    assert 1 + 2 == 2;
   }
 }
